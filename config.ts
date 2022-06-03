e8181c9a6e8b31c2e8e560d7c38e93a0842734ed
Gia <giaendong@gmail.com>
2022-06-03 19:01:14 +0700

    chores(host): add production api host

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 const apiHost = process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000/api'
-    : 'http://localhost:3000/api';
+    : 'https://showwcase-challenge.vercel.app/api';
 
 export {
     apiHost
